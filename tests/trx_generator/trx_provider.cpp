816b9a503b6d3816de301bfbff65d76d4bc705d1
Peter Oschwald <oschwaldp@objectcomputing.com>
2023-10-02 14:46:02 -0500

    Initial port of transaction generator from leap's performance harness.

---
+++ tests/trx_generator/trx_provider.cpp
@@ -0,0 +1,292 @@
+#include <trx_provider.hpp>
+#include <http_client_async.hpp>
+
+#include <fc/io/raw.hpp>
+#include <fc/log/appender.hpp>
+#include <fc/io/json.hpp>
+
+#include <boost/asio.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <eosio/chain/exceptions.hpp>
+
+namespace eosio::testing {
+   using namespace boost::asio;
+   using namespace std::literals::string_literals;
+   using ip::tcp;
+
+   constexpr auto message_header_size = sizeof(uint32_t);
+   constexpr uint32_t packed_trx_which = 8; // this is the "which" for packed_transaction in the net_message variant
+
+   static send_buffer_type create_send_buffer( const chain::packed_transaction& m ) {
+      const uint32_t which_size = fc::raw::pack_size(chain::unsigned_int(packed_trx_which));
+      const uint32_t payload_size = which_size + fc::raw::pack_size( m );
+      const size_t buffer_size = message_header_size + payload_size;
+
+      const char* const header = reinterpret_cast<const char* const>(&payload_size); // avoid variable size encoding of uint32_t
+
+
+      auto send_buffer = std::make_shared<std::vector<char>>(buffer_size);
+      fc::datastream<char*> ds( send_buffer->data(), buffer_size);
+      ds.write( header, message_header_size );
+      fc::raw::pack( ds, fc::unsigned_int(packed_trx_which));
+      fc::raw::pack( ds, m );
+
+      return send_buffer;
+   }
+
+   void provider_connection::init_and_connect() {
+      _connection_thread_pool.start(1, {});
+      connect();
+   };
+
+   void provider_connection::cleanup_and_disconnect() {
+      disconnect();
+      _connection_thread_pool.stop();
+   };
+
+   fc::time_point provider_connection::get_trx_ack_time(const eosio::chain::transaction_id_type& trx_id) {
+      fc::time_point              time_acked;
+      std::lock_guard<std::mutex> lock(_trx_ack_map_lock);
+      auto                        search = _trxs_ack_time_map.find(trx_id);
+      if (search != _trxs_ack_time_map.end()) {
+         time_acked = search->second;
+      } else {
+         elog("get_trx_ack_time - Transaction acknowledge time not found for transaction with id: ${id}",
+              ("id", trx_id));
+         time_acked = fc::time_point::min();
+      }
+      return time_acked;
+   }
+
+   void provider_connection::trx_acknowledged(const eosio::chain::transaction_id_type& trx_id,
+                                              const fc::time_point&                    ack_time) {
+      std::lock_guard<std::mutex> lock(_trx_ack_map_lock);
+      _trxs_ack_time_map[trx_id] = ack_time;
+   }
+
+   void p2p_connection::connect() {
+      ilog("Attempting P2P connection to ${ip}:${port}.", ("ip", _config._peer_endpoint)("port", _config._port));
+      tcp::resolver r(_connection_thread_pool.get_executor());
+      auto i = r.resolve(tcp::v4(), _config._peer_endpoint, std::to_string(_config._port));
+      boost::asio::connect(_p2p_socket, i);
+      ilog("Connected to ${ip}:${port}.", ("ip", _config._peer_endpoint)("port", _config._port));
+   }
+
+   void p2p_connection::disconnect() {
+      ilog("Closing socket.");
+      _p2p_socket.close();
+      ilog("Socket closed.");
+   }
+
+   void p2p_connection::send_transaction(const chain::packed_transaction& trx) {
+      send_buffer_type msg = create_send_buffer(trx);
+      _p2p_socket.send(boost::asio::buffer(*msg));
+      trx_acknowledged(trx.id(), fc::time_point::min()); //using min to identify ack time as not applicable for p2p
+   }
+
+   acked_trx_trace_info p2p_connection::get_acked_trx_trace_info(const eosio::chain::transaction_id_type& trx_id) {
+      return {};
+   }
+
+   void http_connection::connect() {}
+
+   void http_connection::disconnect() {
+      int max    = 30;
+      int waited = 0;
+      while (_sent.load() != _acknowledged.load() && waited < max) {
+         ilog("http_connection::disconnect waiting on ack - sent ${s} | acked ${a} | waited ${w}",
+              ("s", _sent.load())("a", _acknowledged.load())("w", waited));
+         sleep(1);
+         ++waited;
+      }
+      if (waited == max) {
+         elog("http_connection::disconnect failed to receive all acks in time - sent ${s} | acked ${a} | waited ${w}",
+               ("s", _sent.load())("a", _acknowledged.load())("w", waited));
+      }
+   }
+
+   bool http_connection::needs_response_trace_info() {
+      return is_read_only_transaction();
+   }
+
+   bool http_connection::is_read_only_transaction() {
+      return _config._api_endpoint == "/v1/chain/send_read_only_transaction";
+   }
+
+   void http_connection::send_transaction(const chain::packed_transaction& trx) {
+      const int         http_version = 11;
+      const std::string content_type = "application/json"s;
+
+      bool        retry                = false;
+      bool        tx_rtn_failure_trace = true;
+      auto        to_send              = fc::mutable_variant_object()("return_failure_trace", tx_rtn_failure_trace)("retry_trx", retry)("transaction", trx);
+      std::string msg_body             = fc::json::to_string(to_send, fc::time_point::maximum());
+
+      http_client_async::http_request_params params{_connection_thread_pool.get_executor(),
+                                                    _config._peer_endpoint,
+                                                    _config._port,
+                                                    _config._api_endpoint,
+                                                    http_version,
+                                                    content_type};
+      http_client_async::async_http_request(
+          params, std::move(msg_body),
+          [this, trx_id = trx.id()](boost::beast::error_code                                      ec,
+                                    boost::beast::http::response<boost::beast::http::string_body> response) {
+             trx_acknowledged(trx_id, fc::time_point::now());
+             if (ec) {
+                elog("http error: ${c}: ${m}", ("c", ec.value())("m", ec.message()));
+                throw std::runtime_error(ec.message());
+             }
+
+             if (this->needs_response_trace_info() && response.result() == boost::beast::http::status::ok) {
+                try {
+                   fc::variant resp_json = fc::json::from_string(response.body());
+                   if (resp_json.is_object() && resp_json.get_object().contains("processed")) {
+                      const auto& processed      = resp_json["processed"];
+                      const auto& block_num      = processed["block_num"].as_uint64();
+                      const auto& block_time     = processed["block_time"].as_string();
+                      const auto& elapsed_time     = processed["elapsed"].as_uint64();
+                      std::string status         = "failed";
+                      uint32_t    net            = 0;
+                      uint32_t    cpu            = 0;
+                      if (processed.get_object().contains("receipt")) {
+                         const auto& receipt = processed["receipt"];
+                         if (receipt.is_object()) {
+                            status = receipt["status"].as_string();
+                            net    = receipt["net_usage_words"].as_uint64() * 8;
+                            cpu    = receipt["cpu_usage_us"].as_uint64();
+                         }
+                         if (status == "executed") {
+                            record_trx_info(trx_id, block_num, this->is_read_only_transaction() ? elapsed_time : cpu, net, block_time);
+                         } else {
+                            elog("async_http_request Transaction receipt status not executed: ${string}",
+                                 ("string", response.body()));
+                         }
+                      } else {
+                         elog("async_http_request Transaction failed, no receipt: ${string}",
+                              ("string", response.body()));
+                      }
+                   } else {
+                      elog("async_http_request Transaction failed, transaction not processed: ${string}",
+                           ("string", response.body()));
+                   }
+                }
+                EOS_RETHROW_EXCEPTIONS(chain::json_parse_exception, "Fail to parse JSON from string: ${string}",
+                                       ("string", response.body()));
+             }
+
+             if (!(response.result() == boost::beast::http::status::accepted ||
+                   response.result() == boost::beast::http::status::ok)) {
+                elog("async_http_request Failed with response http status code: ${status}",
+                     ("status", response.result_int()));
+             }
+             ++this->_acknowledged;
+          });
+      ++_sent;
+   }
+
+   void http_connection::record_trx_info(const eosio::chain::transaction_id_type& trx_id, uint32_t block_num,
+                                         uint32_t cpu_usage_us, uint32_t net_usage_words,
+                                         const std::string& block_time) {
+      std::lock_guard<std::mutex> lock(_trx_info_map_lock);
+      _acked_trx_trace_info_map.insert({trx_id, {true, block_num, cpu_usage_us, net_usage_words, block_time}});
+   }
+
+   acked_trx_trace_info http_connection::get_acked_trx_trace_info(const eosio::chain::transaction_id_type& trx_id) {
+      acked_trx_trace_info        info;
+      std::lock_guard<std::mutex> lock(_trx_info_map_lock);
+      auto                        search = _acked_trx_trace_info_map.find(trx_id);
+      if (search != _acked_trx_trace_info_map.end()) {
+         info = search->second;
+      } else {
+         elog("get_acked_trx_trace_info - Acknowledged transaction trace info not found for transaction with id: ${id}", ("id", trx_id));
+      }
+      return info;
+   }
+
+   trx_provider::trx_provider(const provider_base_config& provider_config) {
+      if (provider_config._peer_endpoint_type == "http") {
+         _conn.emplace<http_connection>(provider_config);
+         _peer_connection = &std::get<http_connection>(_conn);
+      } else {
+         _conn.emplace<p2p_connection>(provider_config);
+         _peer_connection = &std::get<p2p_connection>(_conn);
+      }
+   }
+
+   void trx_provider::setup() { _peer_connection->init_and_connect(); }
+
+   void trx_provider::send(const chain::signed_transaction& trx) {
+      chain::packed_transaction pt(trx);
+      _peer_connection->send_transaction(pt);
+      _sent_trx_data.push_back(logged_trx_data(trx.id()));
+   }
+
+   void trx_provider::log_trxs(const std::string& log_dir) {
+      std::ostringstream fileName;
+      fileName << log_dir << "/trx_data_output_" << getpid() << ".txt";
+      std::ofstream out(fileName.str());
+
+      for (const logged_trx_data& data : _sent_trx_data) {
+         fc::time_point   acked = _peer_connection->get_trx_ack_time(data._trx_id);
+         std::string      acked_str;
+         fc::microseconds ack_round_trip_us;
+         if (fc::time_point::min() == acked) {
+            acked_str         = "NA";
+            ack_round_trip_us = fc::microseconds(-1);
+         } else {
+            acked_str         = acked.to_iso_string();
+            ack_round_trip_us = acked - data._timestamp;
+         }
+         out << std::string(data._trx_id) << "," << data._timestamp.to_iso_string() << "," << acked_str << ","
+             << ack_round_trip_us.count();
+
+         acked_trx_trace_info info = _peer_connection->get_acked_trx_trace_info(data._trx_id);
+         if (info._valid) {
+            out << "," << info._block_num << "," << info._cpu_usage_us << "," << info._net_usage_words << "," << info._block_time;
+         }
+         out << "\n";
+      }
+      out.close();
+   }
+
+   void trx_provider::teardown() {
+      _peer_connection->cleanup_and_disconnect();
+   }
+
+   bool tps_performance_monitor::monitor_test(const tps_test_stats &stats) {
+      if ((!stats.expected_sent) || (stats.last_run - stats.start_time < _spin_up_time)) {
+         return true;
+      }
+
+      int32_t trxs_behind = stats.expected_sent - stats.trxs_sent;
+      if (trxs_behind < 1) {
+         return true;
+      }
+
+      uint32_t per_off = (100*trxs_behind) / stats.expected_sent;
+
+      if (per_off > _max_lag_per) {
+         if (_violation_start_time.has_value()) {
+           auto lag_duration_us = stats.last_run - _violation_start_time.value();
+           if (lag_duration_us > _max_lag_duration_us) {
+               elog("Target tps lagging outside of defined limits. Terminating test");
+               elog("Expected=${expected}, Sent=${sent}, Percent off=${per_off}, Violation start=${vstart} ",
+                    ("expected",  stats.expected_sent)
+                    ("sent", stats.trxs_sent)
+                    ("per_off", per_off)
+                    ("vstart", _violation_start_time));
+               _terminated_early = true;
+               return false;
+           }
+         } else {
+            _violation_start_time.emplace(stats.last_run);
+         }
+      } else {
+         if (_violation_start_time.has_value()) {
+            _violation_start_time.reset();
+         }
+      }
+      return true;
+   }
+}
---
+++ tests/trx_generator/trx_provider.hpp
@@ -0,0 +1,247 @@
+#pragma once
+
+#include<eosio/chain/transaction.hpp>
+#include<eosio/chain/block.hpp>
+#include<boost/asio/ip/tcp.hpp>
+#include<fc/network/message_buffer.hpp>
+#include<eosio/chain/thread_utils.hpp>
+#include<chrono>
+#include<thread>
+#include<variant>
+#include<vector>
+#include<mutex>
+
+using namespace std::chrono_literals;
+
+namespace eosio::testing {
+   using send_buffer_type = std::shared_ptr<std::vector<char>>;
+
+   struct logged_trx_data {
+      eosio::chain::transaction_id_type _trx_id;
+      fc::time_point _timestamp;
+
+      explicit logged_trx_data(eosio::chain::transaction_id_type trx_id, fc::time_point time_of_interest=fc::time_point::now()) :
+         _trx_id(trx_id), _timestamp(time_of_interest) {}
+   };
+
+   struct provider_base_config {
+      std::string    _peer_endpoint_type = "p2p";
+      std::string    _peer_endpoint      = "127.0.0.1";
+      unsigned short _port               = 9876;
+      // Api endpoint not truly used for p2p connections as transactions are streamed directly to p2p endpoint
+      std::string    _api_endpoint       = "/v1/chain/send_transaction2";
+
+      std::string to_string() const {
+         std::ostringstream ss;
+         ss << "Provider base config endpoint type: " << _peer_endpoint_type << " peer_endpoint: " << _peer_endpoint
+            << " port: " << _port << " api endpoint: " << _api_endpoint;
+         return ss.str();
+      }
+   };
+
+   struct acked_trx_trace_info {
+      bool        _valid           = false;
+      uint32_t    _block_num       = 0;
+      uint32_t    _cpu_usage_us    = 0;
+      uint32_t    _net_usage_words = 0;
+      std::string _block_time      = "";
+
+      std::string to_string() const {
+         std::ostringstream ss;
+         ss << "Acked Transaction Trace Info "
+            << "valid: " << _valid << " block num: " << _block_num << " cpu usage us: " << _cpu_usage_us
+            << " net usage words: " << _net_usage_words << " block time: " << _block_time;
+         return ss.str();
+      }
+   };
+
+   struct provider_connection {
+      const provider_base_config&                                 _config;
+      eosio::chain::named_thread_pool<struct provider_connection> _connection_thread_pool;
+
+      std::mutex _trx_ack_map_lock;
+      std::map<eosio::chain::transaction_id_type, fc::time_point> _trxs_ack_time_map;
+
+      explicit provider_connection(const provider_base_config& provider_config)
+          : _config(provider_config) {}
+
+      virtual ~provider_connection() = default;
+
+      void init_and_connect();
+      void cleanup_and_disconnect();
+      fc::time_point get_trx_ack_time(const eosio::chain::transaction_id_type& trx_id);
+      void trx_acknowledged(const eosio::chain::transaction_id_type& trx_id, const fc::time_point& ack_time);
+
+      virtual acked_trx_trace_info get_acked_trx_trace_info(const eosio::chain::transaction_id_type& trx_id) = 0;
+      virtual void send_transaction(const chain::packed_transaction& trx) = 0;
+
+    private:
+      virtual void connect()    = 0;
+      virtual void disconnect() = 0;
+   };
+
+   struct http_connection : public provider_connection {
+      std::mutex                                                     _trx_info_map_lock;
+      std::map<eosio::chain::transaction_id_type, acked_trx_trace_info> _acked_trx_trace_info_map;
+
+      std::atomic<uint64_t> _acknowledged{0};
+      std::atomic<uint64_t> _sent{0};
+
+      explicit http_connection(const provider_base_config& provider_config)
+          : provider_connection(provider_config) {}
+
+      void send_transaction(const chain::packed_transaction& trx) final;
+      void record_trx_info(const eosio::chain::transaction_id_type& trx_id, uint32_t block_num, uint32_t cpu_usage_us,
+                           uint32_t net_usage_words, const std::string& block_time);
+      acked_trx_trace_info get_acked_trx_trace_info(const eosio::chain::transaction_id_type& trx_id) override final;
+
+    private:
+      void connect() override final;
+      void disconnect() override final;
+      bool needs_response_trace_info();
+      bool is_read_only_transaction();
+   };
+
+   struct p2p_connection : public provider_connection {
+      boost::asio::ip::tcp::socket _p2p_socket;
+
+      explicit p2p_connection(const provider_base_config& provider_config)
+          : provider_connection(provider_config)
+          , _p2p_socket(_connection_thread_pool.get_executor()) {}
+
+      void send_transaction(const chain::packed_transaction& trx) final;
+
+      acked_trx_trace_info get_acked_trx_trace_info(const eosio::chain::transaction_id_type& trx_id) override final;
+
+    private:
+      void connect() override final;
+      void disconnect() override final;
+   };
+
+   struct trx_provider {
+      explicit trx_provider(const provider_base_config& provider_config);
+
+      void setup();
+      void send(const chain::signed_transaction& trx);
+      void log_trxs(const std::string& log_dir);
+      void teardown();
+
+    private:
+      std::variant<std::monostate, http_connection, p2p_connection> _conn;
+      provider_connection*         _peer_connection;
+      std::vector<logged_trx_data> _sent_trx_data;
+   };
+
+   struct tps_test_stats {
+      uint32_t         total_trxs = 0;
+      uint32_t         trxs_left  = 0;
+      uint32_t         trxs_sent  = 0;
+      fc::time_point   start_time;
+      fc::time_point   expected_end_time;
+      fc::time_point   last_run;
+      fc::time_point   next_run;
+      int64_t          time_to_next_trx_us = 0;
+      fc::microseconds trx_interval;
+      uint32_t         expected_sent;
+   };
+
+   constexpr int64_t min_sleep_us                  = 1;
+   constexpr int64_t default_spin_up_time_us       = std::chrono::microseconds(1s).count();
+   constexpr uint32_t default_max_lag_per          = 5;
+   constexpr int64_t default_max_lag_duration_us  = std::chrono::microseconds(1s).count();
+
+   struct null_tps_monitor {
+      bool monitor_test(const tps_test_stats& stats) {return true;}
+   };
+
+   struct tps_performance_monitor {
+      fc::microseconds                    _spin_up_time;
+      uint32_t                            _max_lag_per;
+      fc::microseconds                    _max_lag_duration_us;
+      bool                                _terminated_early;
+      std::optional<fc::time_point>       _violation_start_time;
+
+      explicit tps_performance_monitor(int64_t spin_up_time=default_spin_up_time_us, uint32_t max_lag_per=default_max_lag_per,
+                              int64_t max_lag_duration_us=default_max_lag_duration_us) : _spin_up_time(spin_up_time),
+                              _max_lag_per(max_lag_per), _max_lag_duration_us(max_lag_duration_us), _terminated_early(false) {}
+
+      bool monitor_test(const tps_test_stats& stats);
+      bool terminated_early() {return _terminated_early;}
+   };
+
+   struct trx_tps_tester_config {
+      uint32_t _gen_duration_seconds;
+      uint32_t _target_tps;
+
+      std::string to_string() const {
+         std::ostringstream ss;
+         ss << "Trx Tps Tester Config: duration: " << _gen_duration_seconds << " target tps: " << _target_tps;
+         return ss.str();
+      };
+   };
+
+   template<typename G, typename M>
+   struct trx_tps_tester {
+      std::shared_ptr<G> _generator;
+      std::shared_ptr<M> _monitor;
+      trx_tps_tester_config _config;
+
+      explicit trx_tps_tester(std::shared_ptr<G> generator, std::shared_ptr<M> monitor, const trx_tps_tester_config& tester_config) :
+            _generator(generator), _monitor(monitor), _config(tester_config) {
+      }
+
+      bool run() {
+         if ((_config._target_tps) < 1 || (_config._gen_duration_seconds < 1)) {
+            elog("target tps (${tps}) and duration (${dur}) must both be 1+", ("tps", _config._target_tps)("dur", _config._gen_duration_seconds));
+            return false;
+         }
+
+         if (!_generator->setup()) {
+            return false;
+         }
+
+         tps_test_stats stats;
+         stats.trx_interval = fc::microseconds(std::chrono::microseconds(1s).count() / _config._target_tps);
+
+         stats.total_trxs = _config._gen_duration_seconds * _config._target_tps;
+         stats.trxs_left = stats.total_trxs;
+         stats.start_time = fc::time_point::now();
+         stats.expected_end_time = stats.start_time + fc::microseconds{_config._gen_duration_seconds * std::chrono::microseconds(1s).count()};
+         stats.time_to_next_trx_us = 0;
+
+         bool keep_running = true;
+
+         while (keep_running) {
+            stats.last_run = fc::time_point::now();
+            stats.next_run = stats.start_time + fc::microseconds(stats.trx_interval.count() * (stats.trxs_sent+1));
+
+            if (_generator->generate_and_send()) {
+               stats.trxs_sent++;
+            } else {
+               elog("generator unable to create/send a transaction");
+               if (_generator->stop_on_trx_fail()) {
+                  elog("generator stopping due to trx failure to send.");
+                  break;
+               }
+            }
+
+            stats.expected_sent = ((stats.last_run - stats.start_time).count() / stats.trx_interval.count()) +1;
+            stats.trxs_left--;
+
+            keep_running = ((_monitor == nullptr || _monitor->monitor_test(stats)) && stats.trxs_left);
+
+            if (keep_running) {
+               fc::microseconds time_to_sleep{stats.next_run - fc::time_point::now()};
+               if (time_to_sleep.count() >= min_sleep_us) {
+                  std::this_thread::sleep_for(std::chrono::microseconds(time_to_sleep.count()));
+               }
+               stats.time_to_next_trx_us = time_to_sleep.count();
+            }
+         }
+
+         _generator->tear_down();
+
+         return true;
+      }
+   };
+}
