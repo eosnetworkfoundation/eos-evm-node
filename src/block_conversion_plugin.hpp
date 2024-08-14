367792948bcc9e5e070271b92b2122b4cb20494c
Matias Romeo <matias.romeo@gmail.com>
2024-08-14 02:38:36 -0300

    Process evmtx_v3 to save overhead/storage price in the block extra data

--- src/block_conversion_plugin.hpp
+++ src/block_conversion_plugin.hpp
@@ -12,13 +12,25 @@ struct pushtx {
 };
 EOSIO_REFLECT(pushtx, miner, rlpx)
 
-struct evmtx_v0 {
+struct evmtx_base {
    uint64_t eos_evm_version;
    std::vector<uint8_t> rlpx;
+};
+
+struct evmtx_v1 : evmtx_base {
    uint64_t base_fee_per_gas;
 };
-using evmtx_type = std::variant<evmtx_v0>;
-EOSIO_REFLECT(evmtx_v0, eos_evm_version, rlpx, base_fee_per_gas)
+
+struct evmtx_v3 : evmtx_base {
+   uint64_t overhead_price;
+   uint64_t storage_price;
+};
+
+using evmtx_type = std::variant<evmtx_v1, evmtx_v3>;
+
+EOSIO_REFLECT(evmtx_base, eos_evm_version, rlpx);
+EOSIO_REFLECT(evmtx_v1, base evmtx_base, base_fee_per_gas);
+EOSIO_REFLECT(evmtx_v3, base evmtx_base, overhead_price, storage_price);
 
 struct gas_parameter_type {
     uint64_t gas_txnewaccount = 0;
